161429c0cdc7237dce4285045c876d12cdbc9088
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2024-05-19 22:49:29 +0200

    Add buttons in HTML

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+let firstNumber = 0, operator = '', secondNumber = 0;
+
 function add(a, b) {
-    return a+b;
+    return a + b;
 }
 
-function subtract(a, b){
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(a, b){
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(a, b){
-    return a/b;
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, op, b) {
+    switch (op) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
 }
