7ecabcb5538aec98017c1ffa0c884713b1c137ef
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2024-07-07 15:35:55 +0200

    Make clear and delete buttons function
    And enable more complex calculations.

--- script.js
+++ script.js
@@ -32,8 +32,8 @@ function operate(a, op, b) {
 
 const result = document.querySelector("#result");
 
-function display(num) {
-    result.textContent = num;
+function display() {
+    result.textContent = displayValue;
 }
 
 function numberClicked(num) {
@@ -45,7 +45,8 @@ function numberClicked(num) {
             firstNumber += num;
         }
 
-        display(firstNumber);
+        displayValue = firstNumber;
+        display();
 
     } else {
         if (secondNumber === 0) secondNumber = num;
@@ -54,7 +55,8 @@ function numberClicked(num) {
             secondNumber += num;
         }
 
-        display(secondNumber);
+        displayValue = secondNumber;
+        display();
     }
 
 
@@ -69,14 +71,52 @@ operands.forEach((item) => {
 const operators = document.querySelectorAll(".operator");
 operators.forEach((item) => {
     item.addEventListener('click', () => {
-        operator = item.getAttribute("value");
+        if (firstNumber != 0) {
+            if (operator === '') {
+                operator = item.getAttribute("value");
+            } else {
+                displayValue = operate(firstNumber, operator, secondNumber);
+                display();
+                firstNumber = displayValue;
+                operator = item.getAttribute("value");
+                secondNumber = 0;
+            }
+        }
     });
 });
 
 const equals = document.querySelector(".equals");
 equals.addEventListener('click', () => {
-    display(operate(firstNumber, operator, secondNumber));
+    if (operator != '') {
+        displayValue = operate(firstNumber, operator, secondNumber);
+        display();
+        firstNumber = displayValue;
+        operator = '';
+        secondNumber = 0;
+    }
+});
+
+
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener('click', () => {
     firstNumber = 0;
     operator = '';
     secondNumber = 0;
+    displayValue = 0;
+    display();
 });
+
+const deleteButton = document.querySelector(".delete");
+deleteButton.addEventListener('click', () => {
+    if (secondNumber === 0 && operator === '') {
+        firstNumber = Math.trunc(firstNumber / 10);
+        displayValue = firstNumber;
+        display();
+    } else if (secondNumber === 0 && operator != '') {
+        operator = '';
+    } else if (secondNumber != 0) {
+        secondNumber = Math.trunc(secondNumber / 10);
+        displayValue = secondNumber;
+        display();
+    }
+})
