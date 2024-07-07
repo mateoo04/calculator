c8ec4ec327171d8e23120d1080e6c624ab8fcd31
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2024-07-07 15:14:28 +0200

    Add click listeners and enable simple calculations
    Add click listeners to numbers, operators and
    equal sign. Make possible calculations with
    two numbers.

--- script.js
+++ script.js
@@ -32,11 +32,51 @@ function operate(a, op, b) {
 
 const result = document.querySelector("#result");
 
+function display(num) {
+    result.textContent = num;
+}
+
 function numberClicked(num) {
-    console.log(num);
+    num = Number(num)
+    if (operator === '') {
+        if (firstNumber === 0) firstNumber = num;
+        else {
+            firstNumber *= 10;
+            firstNumber += num;
+        }
+
+        display(firstNumber);
+
+    } else {
+        if (secondNumber === 0) secondNumber = num;
+        else {
+            secondNumber *= 10;
+            secondNumber += num;
+        }
+
+        display(secondNumber);
+    }
+
+
+    console.log(firstNumber, operator, secondNumber);
 }
 
 const operands = document.querySelectorAll(".operand");
 operands.forEach((item) => {
     item.addEventListener('click', () => numberClicked(item.getAttribute("value")));
+});
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach((item) => {
+    item.addEventListener('click', () => {
+        operator = item.getAttribute("value");
+    });
+});
+
+const equals = document.querySelector(".equals");
+equals.addEventListener('click', () => {
+    display(operate(firstNumber, operator, secondNumber));
+    firstNumber = 0;
+    operator = '';
+    secondNumber = 0;
 });
